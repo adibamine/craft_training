4d97949920ccebd6f44160be98b282d810a3270b
adibamine <adibamine1@gmail.com>
2022-03-20 18:01:19 +0100

    Single Responsibility Principle initial

---
+++ src/ma/awb/Main.java
@@ -0,0 +1,21 @@
+package ma.awb;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Book harryPotter = new Book(
+                "Harry Potter",
+                "J.K Rowling",
+                19999,
+                200,
+                "1402894627"
+        );
+        Invoice invoice = new Invoice(
+                harryPotter,
+                3,
+                0,
+                Double.valueOf("0.03")
+        );
+        invoice.printInvoice();
+    }
+}
